84ff3da486681209b649e6e55eaecea7523a27b1
Renato Alencar <renatoalencar.73@gmail.com>
2021-12-13 21:43:53 -0300

    Add test workflow

--- lwt_socks4_unix.mli
+++
@@ -1,9 +0,0 @@
-
-val acquire :
-  Socks4.proxy ->
-  string ->
-  int ->
-  (Lwt_unix.file_descr, string) Lwt_result.t
-(** [acquire proxy hostname port] establishes a connection the
-    proxy and initializes by sending and validating the response. *)
-
